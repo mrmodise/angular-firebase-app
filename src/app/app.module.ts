a50fbc47c4f6a842a78e52f82a7f5b3d1001cb12
mrmodise <modisemorebodi@gmail.com>
2017-08-15 22:33:32 +0200

    added missing modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './components/shared/app-routing.module';
 import { AppComponent } from './components/start/app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import {AdminModule} from './admin/admin.module';
+import { ErrorComponent } from './components/error/error.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    ReactiveFormsModule,
+    AdminModule,
     AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
