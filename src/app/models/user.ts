9ac05e55c0d0d5741456eef0e10625844a6354ed
mrmodise <modisemorebodi@gmail.com>
2017-08-15 22:50:25 +0200

    added user model

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,5 +1,23 @@
 export class User {
-  constructor(public fullName: string,
-              public email: string,
-              public password: string){}
+
+  constructor(public email: string,
+              public password: string) {
+  }
+
+  set _email(email: string) {
+    this.email = email;
+  }
+
+  get _email(): string {
+    return this.email;
+  }
+
+  set _password(password: string) {
+    this.password = password;
+  }
+
+  get _password(): string {
+    return this.password;
+  }
+
 }
