61ad978df3600cee54899754a90e494f89895adc
mrmodise <modisemorebodi@gmail.com>
2017-08-15 23:04:56 +0200

    updated the login form

--- src/app/admin/components/login/login.component.ts
+++ src/app/admin/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,8 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -25,6 +27,14 @@ export class LoginComponent implements OnInit {
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  signUp(): void {
+    this.router.navigate(['/admin/signup']);
+  }
+
+  cancel(): void {
+    this.router.navigate(['/admin']);
   }
 
 }
